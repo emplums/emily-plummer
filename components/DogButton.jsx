cc57d314ff24eda1bcb453c09234883adacf2e51
emplums <emplums@github.com>
2021-10-11 21:20:57 -0500

    add good halloween doggos

--- components/DogButton.jsx
+++ components/DogButton.jsx
@@ -1,23 +1,36 @@
 
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Button from  './Button'
+import styled from 'styled-components'
+
+const StyledImage = styled.img`
+    max-width: 500px;
+    max-height: 500px;
+`
+
 
 const DogButton = ({isHalloween}) => {
-    const dogBreed = isHalloween ? 'borzoi' : 'husky'
     const [dogUrl, setDogUrl] = useState('')
     const getDog = async () => {
-        const dogResponse = await fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+        if (isHalloween) {
+            console.log(isHalloween)
+            const randomNumber = Math.floor(Math.random() * 6) + 1
+            return setDogUrl(`/dog-halloween-${randomNumber}.jpg`)
+        }
+        console.log('hi')
+        const dogResponse = await fetch(`https://dog.ceo/api/breed/husky/images/random`)
         const dogData = await dogResponse.json();
-        console.log(dogData)
         setDogUrl(dogData.message)
     }
 
     const clearDogs = () => setDogUrl('')
+
+    useEffect(() => clearDogs(), [isHalloween])
     return (
         <>
             <Button onClick={getDog} style={{display: 'block', margin: '4px'}}>get a dog</Button>
             {dogUrl.length > 1 && <Button onClick={clearDogs} style={{display: 'block', margin: '4px'}}>no more dogs please</Button>}
-            {dogUrl.length > 1 && <img src={dogUrl}  alt= "a dog"/>}
+            {dogUrl.length > 1 && <StyledImage src={dogUrl}  alt= "a dog"/>}
         </>
     )
 }
