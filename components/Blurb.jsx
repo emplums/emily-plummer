fd614d1645f0d488844f45e63ed7fac8fd500748
emplums <emplums@github.com>
2021-10-11 20:54:33 -0500

    add creepy dogs to halloween theme

--- components/Blurb.jsx
+++ components/Blurb.jsx
@@ -69,7 +69,7 @@ const Blurb = ({themeName}) => {
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/AxcM3nCsglA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             }
 
-            <DogButton/>
+            <DogButton isHalloween={isHalloween}/>
         </StyledBlurb>
     )
 }
--- components/DogButton.jsx
+++ components/DogButton.jsx
@@ -2,10 +2,11 @@
 import React, {useState} from 'react'
 import Button from  './Button'
 
-const DogButton = () => {
+const DogButton = ({isHalloween}) => {
+    const dogBreed = isHalloween ? 'borzoi' : 'husky'
     const [dogUrl, setDogUrl] = useState('')
     const getDog = async () => {
-        const dogResponse = await fetch('https://dog.ceo/api/breed/husky/images/random')
+        const dogResponse = await fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
         const dogData = await dogResponse.json();
         console.log(dogData)
         setDogUrl(dogData.message)
